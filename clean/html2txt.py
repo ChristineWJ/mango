caa5523c01f76a0a12644d68a36591a586637b78
jasonhavend <jasonhaven@163.com>
2018-04-04 00:03:31 +0800

    update html2txt

--- clean/html2txt.py
+++ clean/html2txt.py
@@ -9,11 +9,8 @@
    Change Activity:2018/4/3:
 -------------------------------------------------
 """
-import sys
-import urllib.request
-import chardet
-from log import config
 from crawl import url2html
+from bs4 import BeautifulSoup
 
 
 class cleaner():
@@ -21,16 +18,24 @@ class cleaner():
 		pass
 
 	def clean_html(self, html):
-		pass
+		soup = BeautifulSoup(html, 'html.parser')
+		body = soup.body
+		# text = .replace(re_space, "")
+		body_text = body.get_text()
+		text = []
+		for line in body_text.split("\n"):
+			if len(line.strip()) != 0:
+				text.append(str(line))
+		return text
 
 
 if __name__ == '__main__':
 	headers = {
 	}
-	crawler = url2html.crawler("http://www.baidu.com", headers)
+	crawler = url2html.crawler(
+		"https://baike.baidu.com/item/%E7%BA%AF%E6%96%87%E6%9C%AC%E6%B5%8F%E8%A7%88%E5%99%A8/8789704", headers)
 	html = crawler.get_html()
 
 	cleaner = cleaner()
 	text = cleaner.clean_html(html)
-
 	print(text)
